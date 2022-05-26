ca2c3aa39f364b302475a75c197be9d72a4bac4c
justin <jyi@ucsb.edu>
2022-05-26 13:57:08 -0700

    Login Route

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,43 +1,47 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-const { restoreUser, requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+// const asyncHandler = require('express-async-handler');
+// const { User } = require('../../db/models');
+// const { restoreUser, requireAuth, setTokenCookie } = require('../../utils/auth.js');
 
 
-// GET /api/set-token-cookie
-router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        }
-    });
-    setTokenCookie(res, user);
-    return res.json({ user });
-}));
-
-
-// GET /api/restore-user
-router.get(
-    '/restore-user',
-    restoreUser,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
-
-// GET /api/require-auth
-router.get(
-    '/require-auth',
-    requireAuth,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
+
+// GET /api/set-token-cookie
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// }));
+
+
+// GET /api/restore-user
+// router.get(
+//     '/restore-user',
+//     restoreUser,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+// GET /api/require-auth
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
 
 module.exports = router;
