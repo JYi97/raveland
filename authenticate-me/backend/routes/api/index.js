dd88555c89b4a01593745fb91d92edb5530d9d80
justin <jyi@ucsb.edu>
2022-05-26 13:25:32 -0700

    User Authentication

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,10 +1,43 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser, requireAuth } = require('../../utils/auth.js');
+
+
+// GET /api/set-token-cookie
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
+
+
+// GET /api/restore-user
+router.get(
+    '/restore-user',
+    restoreUser,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
+// GET /api/require-auth
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
 
 module.exports = router;
-
-
-
